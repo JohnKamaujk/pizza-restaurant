ceb6b59ff63457f47f61e2948c0e6c2c7ea4918e
John Kamau <johnjkkamau@gmail.com>
2022-10-05 13:51:33 +0300

    Added relation macros to models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding data..."
+
+Restaurant.create(name: "Sottocasa NYC",address: "298 Atlantic Ave, Brooklyn, NY 11201")
+Restaurant.create(name: "PizzArte",address: "69 W 55th St, New York, NY 10019")
+Restaurant.create(name: "Pizzeria",address: "256th , Boston, Bst 1239")
+Restaurant.create(name: "Guerreiro",address: "23 N 40th St, Los Angeles, LA 345")
+
+Pizza.create(name: "Cheese",ingredients: "Dough, Tomato Sauce, Cheese")
+Pizza.create(name: "Pepperoni",ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+Pizza.create(name: "Mozzarella",ingredients: "Dough, Tomato Sauce, Mozzarella")
+Pizza.create(name: "Chicken",ingredients: "Dough, Tomato Sauce, Cheese, Chicken")
+
+RestaurantPizza.create(price:5,pizza_id:1,restaurant_id:2)
+RestaurantPizza.create(price:18,pizza_id:2,restaurant_id:1)
+RestaurantPizza.create(price:22,pizza_id:3,restaurant_id:4)
+RestaurantPizza.create(price:8,pizza_id:4,restaurant_id:3)
+
+puts "✅ Done seeding!"
