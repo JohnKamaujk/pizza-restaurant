44ebbb1011ad48cab642541f2cc01f631cab6f37
John Kamau <johnjkkamau@gmail.com>
2022-10-05 15:27:11 +0300

    Created restaurant routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,26 @@
 class RestaurantsController < ApplicationController
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         restaurants = Restaurant.all
-        render json: restaurants
+        render json: restaurants, status: :ok
     end
+
+    def show
+        restaurant = find_restaurant
+        render json: restaurant,status: :ok
+    end
+
+    private
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Restaurant not found"}, status: :not_found
+    end
+    
 
 end
